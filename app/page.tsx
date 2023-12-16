15ac34a6359436b7f0195332e0e0949bbdec8d7f
Samuel Prado <samuel.prado.osorio20@gmail.com>
2023-12-15 20:39:33 -0500

    Add dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import styles from './ui/home.module.css'
+import Image from 'next/image';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
@@ -7,6 +9,7 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
+        <div className={styles.shape}></div>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -25,7 +28,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src='/hero-desktop.png'
+            alt='Dashboard screenshots'
+            width={1000}
+            height={760}
+            className='hidden md:block'
+          />
+          <Image
+            src='/hero-mobile.png'
+            alt='Dashboard screenshots'
+            width={560}
+            height={620}
+            className='block md:hidden'
+          />
         </div>
       </div>
     </main>
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Lusitana, Montserrat} from 'next/font/google'
+
+export const montserrat = Montserrat({ subsets: ['latin']})
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700']})
